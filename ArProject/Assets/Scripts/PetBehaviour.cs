eda32ab4586a7b89635c15a1624ff887cf1d672d
Riad <combatarms970711@gmail.com>
2020-01-26 01:23:24 -0500

    Doggie now follows you <3

--- ArProject/Assets/Scripts/PetBehaviour.cs
+++ ArProject/Assets/Scripts/PetBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,12 @@ public class PetBehaviour : MonoBehaviour
 {
     Animator _anim;
 
-    public float speed = 1.01f;
+    private const float MAX_SPEED = 1.25f;
+    private const float RUN_SPEED = 1.01f;
+    private const float IDLE_SPEED = 0f;
+    public float speed = IDLE_SPEED;
+
+    private Camera FpsCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +23,48 @@ public class PetBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        var newPos = Vector3.MoveTowards(transform.position, FpsCamera.transform.position, 0.03f);
+        newPos.y = transform.position.y;
+
+        transform.LookAt(newPos, Vector3.up);
+        transform.position = newPos;
+
+        if((newPos - transform.position).magnitude > 0f)
+        {
+            speed = 0.75f;
+        }else
+        {
+            speed = IDLE_SPEED;
+        }
+
+        FixSpeedGlitch();
         _anim.SetFloat("speed", speed);
     }
 
     public void OnUserClickHit()
     {
         _anim.SetTrigger("jump");
+    }
+
+    private void FixSpeedGlitch()
+    {
+        if (speed > 0.99999f && speed < 1.01f)
+        {
+            speed = RUN_SPEED;
+        }
+        else if (speed > MAX_SPEED)
+        {
+            speed = MAX_SPEED;
+        }
+        else if (speed < IDLE_SPEED)
+        {
+            speed = IDLE_SPEED;
+        }
+    }
+
+    internal void SetFirstPersonCamera(Camera firstPersonCamera)
+    {
+        FpsCamera = firstPersonCamera;
     }
 }
